9402c8613ffa5bca9613032b1d1b02a7ec82eebb
Manya Zhu <manya.zhu@gmail.com>
2022-06-22 15:39:45 -0400

    all .h files first version

---
+++ add.h
@@ -0,0 +1,13 @@
+#pragma once
+/*------------------------------------------------------------*/
+/* add.h.                                                     */
+/* Author: Manya Zhu                                          */
+/*------------------------------------------------------------*/
+
+#include "matrix.h"
+
+/*------------------------------------------------------------*/
+
+/* The add function takes in two matrices as input and returns
+a matrix that is the sum of the two arguments */
+Matrix_t mat_add(Matrix_t mat1, Matrix_t mat2);
---
+++ multiply.h
@@ -0,0 +1,13 @@
+#pragma once
+/*------------------------------------------------------------*/
+/* multiply.h                                                 */
+/* Author: Manya Zhu                                          */
+/*------------------------------------------------------------*/
+
+#include "matrix.h"
+
+/*------------------------------------------------------------*/
+
+/* The multiply function takes in two matrices as input and returns
+a matrix that is the product of the two arguments */
+Matrix_t mat_multiply(Matrix_t mat1, Matrix_t mat2);
