c8901f1e8494a373a22b42dc20f3fbbb3a8a2b77
Dara Oseyemi <doseyemi@princeton.edu>
2022-06-23 14:06:30 -0400

    edit

--- multiply.c
+++ multiply.c
@@ -29,7 +29,7 @@ Matrix_t mat_multiply (Matrix_t mat1, Matrix_t mat2){
         for (int j = 0; j < q; j++){
             *(resultingMatrix.elements + i * q + j) = 0;
             for (int k = 0; k < n; k++){
-                *(resultingMatrix.elements + i * q + j) += *(mat1.elements + i * q + k) + *(mat2.elements + k * q + j);
+                *(resultingMatrix.elements + i * q + j) += *(mat1.elements + i * q + k) * *(mat2.elements + k * q + j);
             }
         }
     }
