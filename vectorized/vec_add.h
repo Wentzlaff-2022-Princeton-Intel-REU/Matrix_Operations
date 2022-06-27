367cd5ad32d3c4bac673422fdc83f1c4da92c866
Manya Zhu <manya.zhu@gmail.com>
2022-06-27 13:56:39 -0400

    Rename

---
+++ vectorized/vec_add.h
@@ -0,0 +1,13 @@
+#pragma once
+/*------------------------------------------------------------*/
+/* vec_add.h                                                  */
+/* Author: Manya Zhu                                          */
+/*------------------------------------------------------------*/
+
+#include "vec_matrix.h"
+
+/*------------------------------------------------------------*/
+
+/* The add function takes in two matrices as input and returns
+a matrix that is the sum of the two arguments */
+Matrix_t vec_add(Matrix_t mat1, Matrix_t mat2);
