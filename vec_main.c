ed710f0cbc8b1f19258d043ae67d9d03ab20f0c8
Dara Oseyemi <doseyemi@princeton.edu>
2022-06-28 15:54:31 -0400

    WE DONE!

--- vec_main.c
+++ vec_main.c
@@ -17,7 +17,7 @@
 int main(int argc, char *argv[]) {
 
     if(argc != 7){
-        fprintf(stderr, "Usage: %s 5 values needed\n", argv[0]);
+        fprintf(stderr, "Usage: %s 6 values needed\n", argv[0]);
         exit(EXIT_FAILURE);
     }
     
